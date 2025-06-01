9a0723d0fbe3b8c3537edb07088ee1936276c0d4
auth_project <helpaword@gmail.com>
2025-05-31 16:22:46 -0800

    test commiting from octopus

--- config/settings.py
+++ config/settings.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+import os
 import django_heroku
 from pathlib import Path
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
