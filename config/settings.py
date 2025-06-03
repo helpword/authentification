130cf681dd279fdb771d60d7c6d497aeebf2e47a
auth_project <helpaword@gmail.com>
2025-06-03 00:02:17 -0800

    smtp

--- config/settings.py
+++ config/settings.py
@@ -131,3 +131,11 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressdManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# settings.py
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'  # أو أي مزود SMTP آخر
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'youremail@gmail.com'
+EMAIL_HOST_PASSWORD = 'your-app-password'  # استخدم "App Password" وليس كلمة مرورك العادية
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
